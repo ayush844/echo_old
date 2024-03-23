24b852006e0f6ae7eb85946b52ff05d26c5fcc1a
ayush844 <ayush.xyz1625@gmail.com>
2024-03-23 17:08:06 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -20,7 +20,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:7000", {
+            const socket = io("https://echo-2hha.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
